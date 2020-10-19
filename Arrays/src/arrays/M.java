0dac67b009f5dfcd987ce12a371255748a76ea2a
Ganga <gangamarad777@gmail.com>
2020-10-19 16:22:49 +0530

    3thanks

--- Arrays/src/arrays/M.java
+++ Arrays/src/arrays/M.java
@@ -4,6 +4,7 @@ public class M {
 	public static void main(String[] args) {
 		System.out.println("Thamk you");
 		System.out.println("Thamk you2");
+		System.out.println("Thamk you2");
 	}
 	class N
 	{
